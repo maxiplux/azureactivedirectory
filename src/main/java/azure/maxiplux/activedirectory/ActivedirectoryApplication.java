2349a9a5b2e6ef20a8b3d05851550b02cafe054c
Juan Mosquera <maxiplux@gmail.com>
2020-09-26 21:59:04 -0400

    Initial commit

---
+++ src/main/java/azure/maxiplux/activedirectory/ActivedirectoryApplication.java
@@ -0,0 +1,13 @@
+package azure.maxiplux.activedirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivedirectoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivedirectoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/azure/maxiplux/activedirectory/ActivedirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package azure.maxiplux.activedirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivedirectoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
